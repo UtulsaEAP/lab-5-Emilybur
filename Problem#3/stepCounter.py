608e5ded5b5083aefb998ed709293e5d4415b899
Emily Burke <erb9078@utulsa.edu>
2024-03-01 15:49:29 -0600

    finished problem 3

--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,8 +1,10 @@
 def feet_to_steps(user_feet):
    #write your code here
-
+   #Emily Burke Lab time friday at 3
+   return user_feet/2.5
 if __name__ == '__main__':
-    #take input feet steps here
-    #store it into the function
-    
-    #print your steps here
+ #take input feet steps here
+ #store it into the function
+   user_feet = float(input())
+   #print your steps here
+   print(f'{feet_to_steps(float(user_feet)):.0f}')
