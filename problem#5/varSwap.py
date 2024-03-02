e2150d03cd38a137083c25ede0c8420f72a512b8
Emily Burke <erb9078@utulsa.edu>
2024-03-01 18:18:08 -0600

    finished problem 5

--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -1,6 +1,13 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   
+   # Emily Burke Lab time Friday at 3
+   temp = user_val1
+   user_val1 = user_val2
+   user_val2 = temp
+
+   temp = user_val3
+   user_val3 = user_val4
+   user_val4 = temp
    return user_val1, user_val2, user_val3, user_val4
 
 if __name__ == '__main__':   
@@ -8,7 +15,15 @@ if __name__ == '__main__':
    user_input2 = int(input())
    user_input3 = int(input())
    user_input4 = int(input())
-   #store output for every input here
-   #print those output
 
+   #store output for every input here
+   output2 = user_input1
+   output1 = user_input2
+   output3 = user_input4
+   output4 = user_input3
+
+   output1, output2, output3, output4 = swap_values(user_input1, user_input2, user_input3, user_input4)
+
+   #print those output
+   print(f'{ str(output1) } { str(output2) } { str(output3) } { str(output4) }')
  
